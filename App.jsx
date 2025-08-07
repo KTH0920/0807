f22e2d268cb5f941f15672172e17b84a4f99e7e7
KTH0920 <taehyun9482@gmail.com>
2025-08-07 14:21:12 +0900

    m

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import FocusInput from './components/FocusInput'
+import Counter from './components/Counter'
+import InputTracker from './components/InputTracker'
+
+function App() {
+
+  return (
+    <div>
+      <FocusInput />
+      <Counter />
+      <InputTracker />
+    </div> 
+  )
+}
+
+export default App
